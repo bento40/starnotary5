93721b1ae336d2ec8656ba332c0859bc9dd94020
bento40 <phantaporntr@gmail.com>
2021-07-07 21:32:53 +0700

    first commit

---
+++ note.js
@@ -0,0 +1,21 @@
+nvm install node --reinstall-packages-from=node
+
+npm install --save-dev sha3
+npm install --save  openzeppelin-solidity
+
+2_deploy_contracts.js
+=====================
+
+   Deploying 'StarNotary'
+   ----------------------
+   > transaction hash:    0x6bc46eafdb41f276d5298fa34478c11884aebc5231a363628200531f1d233c25
+   > Blocks: 1            Seconds: 9
+   > contract address:    0xbbbD3a4029961053c1B550cE5f729b5BC3567520
+   > block number:        8895156
+   > block timestamp:     1625664957
+   > account:             0x33125fd30a1945ad9fc887398195f51dd8a1b940
+   > balance:             1.948189342
+   > gas used:            2282048 (0x22d240)
+   > gas price:           10 gwei
+   > value sent:          0 ETH
+   > total cost:          0.02282048 ETH
